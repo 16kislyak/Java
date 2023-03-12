ee74a6850af9ed10dc9fff9d26bc7655b1e6fb0c
16kislyak <16kislyak@gmail.com>
2023-03-12 17:46:44 +0300

    added lesson1

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,57 @@
+package lesson1;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        sayHello();
+        arrMax();
+
+    }
+
+    private static void sayHello() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = in.nextLine();
+        System.out.println(LocalDateTime.now());
+        LocalDateTime localTime = LocalDateTime.now();
+        System.out.println("Hello, " + name + "!");
+        in.close();
+    }
+
+    //    Дан массив двоичных чисел, например [1,1,0,1,1,1],
+    //    вывести максимальное количество подряд идущих 1.
+    private static void arrMax() {
+        int[] arr = new int[]{1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1};
+        int count = 0;
+        int temp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                temp++;
+            }
+            if (arr[i] == 0 && count < temp) {
+                count = temp;
+                temp = 0;
+            }
+            if (count > temp && arr[i] == 0) {
+                temp = 0;
+            }
+        }
+        if (temp > count) {
+            System.out.println(temp);
+        } else System.out.println(count);
+    }
+
+    private static void putElToEnd() {
+        int [] arr = new int[] {3,2,2,3};
+        int val = 3;
+
+        for (int i = 0; i <  ; i++) {
+
+        }
+    }
+
+}
+
